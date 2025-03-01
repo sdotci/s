895dee6d68890d0f33a725e9c16d578d8753d35d
⚡ Sigui <siguici@proton.me>
2025-03-01 21:06:49 +0100

    🚚 Rename Server and Program to Application

--- src/core/Foundation/Cli/Application.php
+++ src/core/Foundation/Cli/Application.php
@@ -5,11 +5,45 @@ declare(strict_types=1);
 namespace S\Foundation\Cli;
 
 use S\Foundation\Application as BaseApplication;
+use S\Foundation\Result;
 
 class Application extends BaseApplication
 {
-    public function run(): void
+    protected array $commands = [];
+
+    public function __construct(protected Context $context) {}
+
+    /**
+     * @param  array<callable-string|callable-object>|array{object|class-string,string}|callable-string|callable-object|callable(mixed ...$args): mixed  $handler
+     */
+    public function on(string $signature, array|string|object|callable $handler, ?string $name = null, ?string $description = null): Command
     {
-        echo 'Welcome to S CLI Application!'.PHP_EOL;
+        return $this->add(new Command($signature, $handler, $name, $description));
+    }
+
+    public function add(Command $command): Command
+    {
+        $this->commands[] = $command;
+
+        return $command;
+    }
+
+    public function run(): never
+    {
+        $input = $this->context->getInput();
+
+        $arg0 = $input->get(0);
+
+        $binFile = realpath($arg0);
+        $input->remove(0);
+
+        foreach ($this->commands as $command) {
+            if ($command->match(implode(' ', $input->getData()))) {
+                $command->resolve()->send();
+            }
+        }
+
+        $result = new Result('No command matched'.PHP_EOL);
+        $result->send();
     }
 }
--- src/core/Foundation/Http/Application.php
+++ src/core/Foundation/Http/Application.php
@@ -5,11 +5,46 @@ declare(strict_types=1);
 namespace S\Foundation\Http;
 
 use S\Foundation\Application as BaseApplication;
+use S\Foundation\Input;
 
 class Application extends BaseApplication
 {
+    protected Input $input;
+
+    protected array $routes = [];
+
+    public function __construct(protected Context $context) {}
+
+    /**
+     * @param  array<callable-string|callable-object>|array{object|class-string,string}|callable-string|callable-object|callable(mixed ...$args): mixed  $handler
+     */
+    public function on(string $signature, array|string|object|callable $handler, ?string $name = null, ?string $description = null): Route
+    {
+        return $this->add(new Route($signature, $handler, $name, $description));
+    }
+
+    public function add(Route $route): Route
+    {
+        $this->routes[] = $route;
+
+        return $route;
+    }
+
     public function run(): void
     {
-        echo '<p>Welcome to S HTTP Application!</p>';
+        $input = $this->context->getInput();
+
+        $method = $input->get('REQUEST_METHOD', 'GET');
+        $uri = $input->get('REQUEST_URI', '/');
+
+        $startLine = "{$method} {$uri}";
+
+        foreach ($this->routes as $route) {
+            if ($route->match($startLine)) {
+                $route->resolve()->send();
+
+                return;
+            }
+        }
     }
 }
